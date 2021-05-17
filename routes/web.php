3764d512c3ec8a589a9e8446925598898c3555ec
“MaxLeg” <stalker-999@mail.ru>
2021-05-18 00:42:23 +0300

    Поставил все для аутентификации.

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 Route::get('/', [MainController::class, 'index'])->name('Home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
